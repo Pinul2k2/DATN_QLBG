86de0fd00a5f3a8cd09b39d18fd0cb4ec9a832a8
longnguyen05 <longpu202@gmail.com>
2023-04-24 21:29:07 +0700

    hhhh

--- admincp/config.php
+++ admincp/config.php
@@ -1,9 +1,24 @@
 <?php
-$mysqli = new mysqli("localhost","root","","datn_qlbg");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbName = "datn_qlbg";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+$sql = "SELECT * FROM san_pham order by created_at ";
+
 
 // Check connection
-if ($mysqli->connect_errno) {
-  echo "Kết nối MYSQLi lỗi " . $mysqli->connect_error;
-  exit();
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} else {
+  echo "Connected successfully";
+
+
 }
-?>
+$result = mysqli_query($conn, $sql);
+$san_pham = mysqli_fetch_all($result, MYSQLI_ASSOC);
+mysqli_free_result($result);
+mysqli_close($conn);
+var_dump($san_pham);
