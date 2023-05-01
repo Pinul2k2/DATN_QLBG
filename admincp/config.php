b6b4824ee5006ff0576f19a911b0d234ffa6be77
pinul2k2 <tchauphuc2002@gmail.com>
2023-05-01 21:15:00 +0700

    'signup

--- admincp/config.php
+++ admincp/config.php
@@ -2,23 +2,17 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbName = "datn_qlbg";
+$dbName = "datb_qlbg";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbName);
 
-$sql = "SELECT * FROM san_pham order by created_at ";
+
+
 
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} else {
-  echo "Connected successfully";
+} 
 
 
-}
-$result = mysqli_query($conn, $sql);
-$san_pham = mysqli_fetch_all($result, MYSQLI_ASSOC);
-mysqli_free_result($result);
-mysqli_close($conn);
-var_dump($san_pham);
